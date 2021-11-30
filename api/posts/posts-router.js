0ea92a56894bd33ebb65344af61d5aecde73191c
Jeong Pak <dannypak716@gmail.com>
2021-11-30 15:02:07 -0500

    post and put won't return the new object/new updated objects. will try writing it es6 way and see if that works

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -33,17 +33,46 @@ router.get('/:id', async (req, res) => {
     }
 });
 
-router.get('/', async (req, res) => {
+router.post('/', async (req, res) => {
     try {
-        const posts = await Post.find()
-        res.json(posts)
+        if (!req.body.title || !req.body.contents) {
+            res.status(400).json({
+                message: "Please provide title and contents for the post",
+            })
+        } else {
+            const newPost = await Post.insert(req.body.title, req.body.contents)
+            
+        }
     } catch (err) {
         res.status(500).json({
-            message: "The posts information could not be retrieved",
+            message: "There was an error while saving the post to the database",
             error: err.message
         })
     }
 });
+
+router.put('/:id', async (req, res) => {
+    try {
+        const updatedPost = await Post.update(req.params.id, req.body)
+        if(!updatedPost) {
+            res.status(404).json({
+                message:"The post with the specified ID does not exist"
+            })
+        } else if(!req.body.title || !req.body.contents) {
+            res.status(400).json({
+                message: "Please provide title and contents for the post"
+            })
+        } else {
+            res.status(201).json(updatedPost) 
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: "The post information could not be modified",
+            error: err.message
+        })
+    }
+});
+
 
 
 
